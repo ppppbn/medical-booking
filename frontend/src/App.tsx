c74593046a5e7666793bac0f27f70bd9ba2891ba
NP <phambaongoc1511@gmail.com>
2026-08-10 09:57:41 +0700

    Remove Outfit font and default to Roboto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -45,7 +45,7 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: '"Outfit", "Roboto", "Helvetica", "Arial", sans-serif',
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
     h1: { fontWeight: 700 },
     h2: { fontWeight: 600 },
     h3: { fontWeight: 600 },
