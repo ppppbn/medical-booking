971b7b62bd15892cd26ea83be6df1a1d5d733da9
NP <phambaongoc1511@gmail.com>
2026-09-16 12:21:58 +0700

    Re-provision AWS resources

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -17,7 +17,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-/*
 # IAM Role for SSM
 resource "aws_iam_role" "ssm_role" {
   name = "medbooking-ec2-ssm-role"
@@ -163,4 +162,3 @@ resource "aws_eip" "medbooking_eip" {
     Name = "MedicalBooking-EIP"
   }
 }
-*/
